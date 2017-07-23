7a64d7da5dbed435bfe8cfeb71ca936984eac441
Ahmed Karim <ahmed.karim.lahmar@gmail.com>
2017-07-23 15:49:11 +0100

    status settings

--- app/src/main/java/com/dev/kallinikos/chatbat/SettingsActivity.java
+++ app/src/main/java/com/dev/kallinikos/chatbat/SettingsActivity.java
@@ -1,13 +1,77 @@
 package com.dev.kallinikos.chatbat;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class SettingsActivity extends AppCompatActivity {
+
+    private DatabaseReference mUserDatabase;
+    private FirebaseUser mCurrentUser;
+
+
+    private CircleImageView mImage;
+    private TextView mName;
+    private TextView mStatus;
+
+    private Button mStatusBtn;
+    private Button mImageBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        mImage = (CircleImageView) findViewById(R.id.settings_img);
+        mName = (TextView) findViewById(R.id.settings_display_name);
+        mStatus = (TextView)findViewById(R.id.settings_status);
+        mStatusBtn = (Button)findViewById(R.id.settings_status_btn);
+
+        mCurrentUser = FirebaseAuth.getInstance().getCurrentUser();
+        String currentUid = mCurrentUser.getUid();
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("Users").child(currentUid);
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                String name = dataSnapshot.child("name").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String thumbImage = dataSnapshot.child("thumb_image").getValue().toString();
+
+                mName.setText(name);
+                mStatus.setText(status);
+                //if(image)
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        mStatusBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent statusIntent = new Intent(SettingsActivity.this, StatusActivity.class);
+                String statusValue = mStatus.getText().toString();
+                statusIntent.putExtra("status",statusValue);
+                startActivity(statusIntent);
+            }
+        });
     }
 }
