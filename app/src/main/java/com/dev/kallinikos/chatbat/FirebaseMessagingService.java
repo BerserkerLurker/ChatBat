22c0156bf8ef4a65c52aa5eab5d5bf29c2443fc0
BerserkerLurker <ahmed.karim.lahmar@gmail.com>
2017-12-22 12:06:11 +0100

    Add Firebase notifications on friend request

--- app/src/main/java/com/dev/kallinikos/chatbat/FirebaseMessagingService.java
+++ app/src/main/java/com/dev/kallinikos/chatbat/FirebaseMessagingService.java
@@ -1,6 +1,8 @@
 package com.dev.kallinikos.chatbat;
 
 import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
 
 import com.google.firebase.messaging.RemoteMessage;
@@ -15,11 +17,33 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
     public void onMessageReceived(RemoteMessage remoteMessage) {
         super.onMessageReceived(remoteMessage);
 
+
+        String notification_title = remoteMessage.getNotification().getTitle();
+        String notification_message = remoteMessage.getNotification().getBody();
+
+        String click_action = remoteMessage.getNotification().getClickAction();
+
+        String from_user_id = remoteMessage.getData().get("from_user_id");
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.mipmap.ic_chatbat_round)
-                        .setContentTitle("New Friend Request")
-                        .setContentText("You've received a new Friend Request");
+                        .setContentTitle(notification_title)
+                        .setContentText(notification_message);
+
+
+        Intent resultIntent = new Intent(click_action);
+        resultIntent.putExtra("userid", from_user_id);
+
+        PendingIntent resultPendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        resultIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        mBuilder.setContentIntent(resultPendingIntent);
 
 
         // Sets an ID for the notification
